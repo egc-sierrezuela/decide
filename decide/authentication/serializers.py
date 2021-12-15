792fc3e1dd2eff0890aead93b2e4aba881f93802
fergongar1 <fergongar1@alum.us.es>
2021-12-15 20:36:41 +0100

    Prueba

--- decide/authentication/serializers.py
+++ decide/authentication/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from django.contrib.auth.models import User
 
-
+#prueba
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
