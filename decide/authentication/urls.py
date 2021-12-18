b621824567276bca2917478e4a93279f24efae03
fergongar1 <73081243+fergongar1@users.noreply.github.com>
2021-12-18 21:25:32 +0100

    Auth-03: Bugfix

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -6,12 +6,12 @@ from .views import GetUserViewAPI, LogoutViewAPI, RegisterViewAPI, RegisterView,
 
 
 urlpatterns = [
-    path('login-api/', obtain_auth_token),
-    path('logout-api/', LogoutViewAPI.as_view()),
-    path('getuser-api/', GetUserViewAPI.as_view()),
-    path('register-api/', RegisterViewAPI.as_view()),
-    path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view(),name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path('login-success', SuccessView.as_view(),name="login-success")
+    path('login/', obtain_auth_token),
+    path('logout/', LogoutViewAPI.as_view()),
+    path('getuser/', GetUserViewAPI.as_view()),
+    path('register/', RegisterViewAPI.as_view()),
+    path('register-alt/', RegisterView.as_view()),
+    path('login-alt/', LoginView.as_view(),name="login"),
+    path("logout-alt/", auth_views.LogoutView.as_view(), name="logout"),
+    path('login-success/', SuccessView.as_view(),name="login-success")
 ]
