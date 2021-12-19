766d37775b0693e253d6291a7c7b39768b41aefd
Maria Garcia <margarcac1@alum.us.es>
2021-12-19 13:54:28 +0100

    MOSTRAR INFORMACIÓN EN TIEMPO REAL
    Se ha implementado una modificación en Visualizaciones, concretamente, ahora se puede conocer información en tiempo real relativa al estado, censo total, porcentaje de participación y número de votos de una votación sin finalizar.
    Para poder visualizar el cambio, iniciar una votación y acceder a /visualizer/idDeLaVotacion.
    
    García Cáceres, María <@margarcac1>

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -2,6 +2,8 @@ import json
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.http import Http404
+from census.models import Census
+from store.models import Vote
 
 from base import mods
 
@@ -16,7 +18,24 @@ class VisualizerView(TemplateView):
         try:
             r = mods.get('voting', params={'id': vid})
             context['voting'] = json.dumps(r[0])
-        except:
+
+            if r[0]['end_date'] is None:
+                stats = {}
+                stats['census_size'] = getEstadisticas('census_size', vid)
+                stats['voters_turnout'] = getEstadisticas('turnout', vid)
+                stats['participation_ratio'] = round((stats['voters_turnout']/stats['census_size'])*100,2)
+                for i,j in stats.items():
+                    context['stats_' + str(i)] = j
+
+        except Exception:
             raise Http404
 
         return context
+
+def getEstadisticas(requerido, vid):
+    if (requerido=="census_size"):
+        return Census.objects.filter(voting_id=vid).count()
+    elif (requerido=="turnout"):
+        return Vote.objects.filter(voting_id=vid).count()
+    else:
+        return None
