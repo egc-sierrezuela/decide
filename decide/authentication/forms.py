b5113ea85f4bd96e85192c49505901734ec178a9
ivaromcos-us <ivaromcos@alum.us.es>
2021-12-20 21:46:45 +0100

    Implementación de la funcionalidad de Captcha

--- decide/authentication/forms.py
+++ decide/authentication/forms.py
@@ -1,7 +1,9 @@
 # -*- encoding: utf-8 -*-
 from django import forms
+from captcha.fields import ReCaptchaField
 
 class RegisterForm(forms.Form):
     username = forms.CharField(label='Usuario',widget=forms.TextInput,required=True)
     email = forms.CharField(label='Email',widget=forms.EmailInput,required=True)
     password = forms.CharField(label='Contraseña',widget=forms.PasswordInput,required=True)
+    captcha = ReCaptchaField()
