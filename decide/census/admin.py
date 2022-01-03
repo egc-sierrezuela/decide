6848a05daf8b6fd8a44b0c2481179d9a6d33f6da
raupargor <raupargor@alum.us.es>
2022-01-03 17:57:03 +0100

    IMPORTACION DE CENSO TRANSLADADA A /ADMIN
    Se ha pasado la funcionalidad de importar el censo en una accion del /admin/census/censuss,
     ya que debe estar accesible por personas registradas como administrador,
    es un sitio mas adecuado para esta funcionalidad
    
    Parrado Gordon, Raul<@raupargor>

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
+from django.shortcuts import render,render_to_response
 
 from .models import Census
+from django.http import HttpResponse
+from . import views
 
+def import_census(modeladmin,request,queryset):
+    return render(request,'censusImport.html')
 
 class CensusAdmin(admin.ModelAdmin):
     list_display = ('voting_id', 'voter_id')
     list_filter = ('voting_id', )
 
     search_fields = ('voter_id', )
-
+    actions = [import_census]
 
 admin.site.register(Census, CensusAdmin)
+
