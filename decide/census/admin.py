65072849903f92ac4c129fe3879f0370167ead99
jesrolcad <jesrolcad@alum.us.es>
2022-01-03 20:40:39 +0100

    MENSAJE DE EXPORTACIÓN DE CENSO AÑADIDO
    Se ha añadido un mensaje que se muestra al exportar censos.
    Este mensaje solo se muestra si se recarga la página
    
    Roldán Cadena, Jesús <@jesrolcad>

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -1,29 +1,30 @@
 from django.contrib import admin
 from .models import Census
 from django.http import HttpResponse
+from django.contrib import messages
 import csv
-
-
-
-def export_census(modeladmin, request, queryset):
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="censo.csv"'
-    file = csv.writer(response)
-    file.writerow(['Id votante', 'Id votación'])
-    ids = []
-    for c in queryset:
-        #if c.voter_id not in ids:
-            file.writerow([c.voter_id, c.voting_id])
-            ids.append(c.voter_id)
-
-    return response
 
 class CensusAdmin(admin.ModelAdmin):
     list_display = ('voting_id', 'voter_id')
     list_filter = ('voting_id', )
 
     search_fields = ('voter_id', )
-    actions = [export_census]
+    actions = ['export_census']
+
+    def export_census(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="censo.csv"'
+        file = csv.writer(response)
+        file.writerow(['Id votante', 'Id votación'])
+        ids = []
+        for c in queryset:
+            #if c.voter_id not in ids:
+                file.writerow([c.voter_id, c.voting_id])
+                ids.append(c.voter_id)
+
+        self.message_user(request, "Exportación realizada con éxito", level=messages.SUCCESS)
+
+        return response
 
 
 admin.site.register(Census, CensusAdmin)
