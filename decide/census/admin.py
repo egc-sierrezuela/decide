e2571c7494e145c13cbfb3c2e9b727081e806033
jesrolcad <jesrolcad@alum.us.es>
2022-01-04 20:53:07 +0100

    AÑADIDO FILTRO POR GÉNERO
    Se ha añadido un filtro en la parte de administración de censo
    que permite filtrar los censos por género masculino o femenino
    
    Roldán Cadena, <@jesrolcad>

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -5,13 +5,51 @@ from django.http import HttpResponse
 from . import views
 from django.contrib import messages
 import csv
+from django.contrib.auth.models import User
 
+
+class SexCensusFilter(admin.SimpleListFilter):
+    title = "Filtro por género"  # a label for our filter
+    parameter_name = 'genero'
+
+    def lookups(self, request, model_admin):
+        # This is where you create filter options; we have two:
+        return [
+            ("masculino", "Masculino"),
+            ("femenino", "Femenino"),
+        ]
+
+    def queryset(self, request, queryset):
+        # This is where you process parameters selected by use via filter options:
+        if self.value() == "masculino":
+            # Para cada censo, busco el user y veo su género. Si el género es masculino
+            # entonces añado el c.voting_id en una lista, y después retorno 
+            # aquellos objetos mediante el filtro voting_id__in = lista
+            lista_censos_masculinos = []
+            for c in queryset:
+                usuario = User.objects.get(id=c.voter_id)
+                persona = usuario.persona
+                print(persona.sexo)
+                if persona.sexo == "masculino":
+                    lista_censos_masculinos.append(c.voter_id)
+            print(lista_censos_masculinos)
+            return queryset.filter(voter_id__in=lista_censos_masculinos)
+
+        if self.value() == "femenino":
+            lista_censos_femeninos = []
+            for c in queryset:
+                usuario = User.objects.get(id=c.voter_id)
+                print(usuario)
+                persona = usuario.persona
+                if persona.sexo == "femenino":
+                    lista_censos_femeninos.append(c.voter_id)
+            return queryset.filter(voter_id__in=lista_censos_femeninos)
 
 class CensusAdmin(admin.ModelAdmin):
     list_display = ('voting_id', 'voter_id')
-    list_filter = ('voting_id', )
+    list_filter = ('voting_id', SexCensusFilter)
 
-    search_fields = ('voter_id', )
+    search_fields = ('voter_id',)
 
     actions = ['import_census','export_census']
 
@@ -32,6 +70,7 @@ class CensusAdmin(admin.ModelAdmin):
 
     def import_census(self,request,queryset):
         return render(request,'censusImport.html')
+
 
 admin.site.register(Census, CensusAdmin)
 
