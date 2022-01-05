e53be2f65fbdeb3aee3b64185d6ce3a7ef80c89b
jesrolcad <jesrolcad@alum.us.es>
2022-01-05 21:10:41 +0100

    MODIFICACIÓN TESTS DE CENSO
    Se han modificado los tests de censo. Se han añadido comentarios
    aclaratorios al test de exportar censo y se han descomentado los
    tests de la API que funcionan correctamente.
    
    Roldán Cadena, <@jesrolcad>

--- decide/census/tests.py
+++ decide/census/tests.py
@@ -12,71 +12,71 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-# class CensusTestCase(BaseTestCase):
+class CensusTestCase(BaseTestCase):
 
-#     def setUp(self):
-#         super().setUp()
-#         self.census = Census(voting_id=1, voter_id=1)
-#         self.census.save()
+    def setUp(self):
+        super().setUp()
+        self.census = Census(voting_id=1, voter_id=1)
+        self.census.save()
 
-#     def tearDown(self):
-#         super().tearDown()
-#         self.census = None
+    def tearDown(self):
+        super().tearDown()
+        self.census = None
 
-#     def test_check_vote_permissions(self):
-#         response = self.client.get('/census/{}/?voter_id={}'.format(1, 2), format='json')
-#         self.assertEqual(response.status_code, 401)
-#         self.assertEqual(response.json(), 'Invalid voter')
+    def test_check_vote_permissions(self):
+        response = self.client.get('/census/{}/?voter_id={}'.format(1, 2), format='json')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), 'Invalid voter')
 
-#         response = self.client.get('/census/{}/?voter_id={}'.format(1, 1), format='json')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(response.json(), 'Valid voter')
+        response = self.client.get('/census/{}/?voter_id={}'.format(1, 1), format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), 'Valid voter')
 
-#     def test_list_voting(self):
-#         response = self.client.get('/census/?voting_id={}'.format(1), format='json')
-#         self.assertEqual(response.status_code, 401)
+    # def test_list_voting(self):
+    #     # response = self.client.get('/census/?voting_id={}'.format(1), format='json')
+    #     # self.assertEqual(response.status_code, 401)
 
-#         self.login(user='noadmin')
-#         response = self.client.get('/census/?voting_id={}'.format(1), format='json')
-#         self.assertEqual(response.status_code, 403)
+    #     self.login(user='noadmin')
+    #     response = self.client.get('/census/?voting_id={}'.format(1), format='json')
+    #     self.assertEqual(response.status_code, 403)
 
-#         self.login()
-#         response = self.client.get('/census/?voting_id={}'.format(1), format='json')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(response.json(), {'voters': [1]})
+    #     self.login()
+    #     response = self.client.get('/census/?voting_id={}'.format(1), format='json')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(response.json(), {'voters': [1]})
 
-#     def test_add_new_voters_conflict(self):
-#         data = {'voting_id': 1, 'voters': [1]}
-#         response = self.client.post('/census/', data, format='json')
-#         self.assertEqual(response.status_code, 401)
+    # def test_add_new_voters_conflict(self):
+    #     data = {'voting_id': 1, 'voters': [1]}
+    #     # response = self.client.post('/census/', data, format='json')
+    #     # self.assertEqual(response.status_code, 401)
 
-#         self.login(user='noadmin')
-#         response = self.client.post('/census/', data, format='json')
-#         self.assertEqual(response.status_code, 403)
+    #     self.login(user='noadmin')
+    #     response = self.client.post('/census/', data, format='json')
+    #     self.assertEqual(response.status_code, 403)
 
-#         self.login()
-#         response = self.client.post('/census/', data, format='json')
-#         self.assertEqual(response.status_code, 409)
+    #     self.login()
+    #     response = self.client.post('/census/', data, format='json')
+    #     self.assertEqual(response.status_code, 409)
 
-#     def test_add_new_voters(self):
-#         data = {'voting_id': 2, 'voters': [1,2,3,4]}
-#         response = self.client.post('/census/', data, format='json')
-#         self.assertEqual(response.status_code, 401)
+    # def test_add_new_voters(self):
+    #     data = {'voting_id': 2, 'voters': [1,2,3,4]}
+    #     # response = self.client.post('/census/', data, format='json')
+    #     # self.assertEqual(response.status_code, 401)
 
-#         self.login(user='noadmin')
-#         response = self.client.post('/census/', data, format='json')
-#         self.assertEqual(response.status_code, 403)
+    #     self.login(user='noadmin')
+    #     response = self.client.post('/census/', data, format='json')
+    #     self.assertEqual(response.status_code, 403)
 
-#         self.login()
-#         response = self.client.post('/census/', data, format='json')
-#         self.assertEqual(response.status_code, 201)
-#         self.assertEqual(len(data.get('voters')), Census.objects.count() - 1)
+    #     self.login()
+    #     response = self.client.post('/census/', data, format='json')
+    #     self.assertEqual(response.status_code, 201)
+    #     self.assertEqual(len(data.get('voters')), Census.objects.count() - 1)
 
-#     def test_destroy_voter(self):
-#         data = {'voters': [1]}
-#         response = self.client.delete('/census/{}/'.format(1), data, format='json')
-#         self.assertEqual(response.status_code, 204)
-#         self.assertEqual(0, Census.objects.count())
+    def test_destroy_voter(self):
+        data = {'voters': [1]}
+        response = self.client.delete('/census/{}/'.format(1), data, format='json')
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(0, Census.objects.count())
 
 class ExportCensus(StaticLiveServerTestCase):
 
@@ -84,7 +84,7 @@ class ExportCensus(StaticLiveServerTestCase):
         self.base = BaseTestCase()
         self.base.setUp()
         options = webdriver.ChromeOptions()
-        options.headless = False
+        options.headless = True
         self.driver = webdriver.Chrome(options=options)
         self.vars = {}
         self.census=Census(voting_id=1,voter_id=1)
@@ -118,25 +118,26 @@ class ExportCensus(StaticLiveServerTestCase):
         dropdown = self.driver.find_element(By.NAME, "action")
         dropdown.find_element(By.XPATH, "//option[. = 'Export census']").click()
 
-        # # 6 | click | css=option:nth-child(3) | 
-        # self.driver.find_element(By.CSS_SELECTOR, "option:nth-child(4)").click()
-
-        # 7 | click | name=index | 
-
-        self.driver.implicitly_wait(10)
+        
         element = self.driver.find_element(By.NAME, "action")
         actions = ActionChains(self.driver)
         actions.move_to_element(element).click_and_hold().perform()
+
         element = self.driver.find_element(By.NAME, "action")
         actions = ActionChains(self.driver)
         actions.move_to_element(element).perform()
+
         element = self.driver.find_element(By.NAME, "action")
         actions = ActionChains(self.driver)
         actions.move_to_element(element).release().perform()
+
+        # 6 | click | name=index | 
         self.driver.find_element(By.NAME, "index").click()
 
+        #Refrescamos la página
         self.driver.refresh()
 
+        #Si la exportación se ha realizado, debe aparecer un mensaje
         mensaje = self.driver.find_element(By.CLASS_NAME, "success").text
 
         self.assertEquals(mensaje, 'Exportación realizada con éxito')
