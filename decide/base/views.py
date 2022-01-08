d18c5df5cd11a87eb6abfc1b03e83c2bfd71370a
jorruirui1 <jorruirui1@alum.us.es>
2022-01-08 20:40:10 +0100

    Base-01: Creación de la página de inicio
    Se ha creado la página de inicio junto con una barra de navegación y enlaces a instrucciones de las diferentes funciones que tiene decide

--- decide/base/views.py
+++ decide/base/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'inicio.html'
+
+class VotingInstructionsView(TemplateView):
+    template_name = 'voting.html'
+
+class BoothInstrucionsView(TemplateView):
+    template_name = 'booth.html'
+
+class VisualizerInstructionsView(TemplateView):
+    template_name = 'visualizer.html'
