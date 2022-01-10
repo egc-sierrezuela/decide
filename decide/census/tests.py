bbd4b0047d5975c42ca5396be24fbefa7936c30c
serfiggom <72518706+Servandofg12@users.noreply.github.com>
2022-01-10 17:43:39 +0100

    Update tests.py

--- decide/census/tests.py
+++ decide/census/tests.py
@@ -8,10 +8,6 @@ from .models import Census
 from base import mods
 from base.tests import BaseTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 import csv
 
 class CensusTestCase(BaseTestCase):
